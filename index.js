512acbfd024da4330d86dba3f6679de674f886ae
Ethan-liao <ethan-liao@outlook.com>
2018-09-11 00:32:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 // Write your code in this file!
 function scuberGreetingForFeet (feet) {
+  if ( feet > 2500 ){
+    return 'No can do.'
+  }
+  
   if (feet < 400){
     return "This one is on me!"
   } else if ( feet > 2000) {
     return 'I will gladly take your thirty bucks.'
   } 
-  if ( feet > 2500 ){
-    return 'No can do.'
-  }
+  
   
 }
 
